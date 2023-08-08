ac9886d8cc785f29dda48421d40388f8f4a965a7
furasshujipushii <enzogireauds@gmail.com>
2023-08-08 13:14:46 +0200

    deleting rapier

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
 	import { Canvas } from '@threlte/core';
-	import { World } from '@threlte/rapier';
 	import Scene from './Scene.svelte';
 	import FallbackScene from './FallbackScene.svelte';
 </script>
 
 <Canvas>
-	<World>
-		<Scene />
-		<FallbackScene slot="fallback" />
-	</World>
+	<Scene />
 </Canvas>
